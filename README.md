7f3801cfd838d4d6dc0179071b6aef697c67d74a
Anon4You <alienkrishn@gmail.com>
2025-01-20 00:51:31 +0530

    telegram bot

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ChatIDBot
-Chat id bot for telegram 
+Chat id bot for telegram
+
+
+## installation
+
+```bash
+git clone https://github.com/Anon4You/ChatIDBot.git 
+cd ChatIDBot
+apt install python
+pip install telebot
+
+```
+
+> open bot.py with any text editor and add your bot token 
+
+run the bot by `python bot.py`
---
+++ bot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import telebot
+bot_token = "Your bot token"
+
+bot = telebot.TeleBot(bot_token)
+
+@bot.message_handler(commands=['start'])
+
+def wlcm_msg(message):
+    wlcm_text = "welcom to chat id bot made send /id command to know your id\n created by @alienkrishn"
+    bot.reply_to(message, wlcm_text)
+
+@bot.message_handler(commands=['id'])
+def send_id(message):
+    chat_id = message.chat.id 
+    bot.reply_to(message, f"Your chat id is : {chat_id}")
+bot.polling()
