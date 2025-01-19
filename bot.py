4658f68c20b710adc7e62250d5dde8c45f27514f
Alienkrishn <katilchora666@gmail.com>
2025-01-20 01:20:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 import telebot
-bot_token = "Your bot token"
+bot_token = "Your bot token" # add your telegram bot token
 
 bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['start'])
 
 def wlcm_msg(message):
-    wlcm_text = "welcom to chat id bot made send /id command to know your id\n created by @alienkrishn"
+    wlcm_text = """welcom to chat id bot send /id command to know your id
+    created by @alienkrishn"""" # add your username 
     bot.reply_to(message, wlcm_text)
 
 @bot.message_handler(commands=['id'])
